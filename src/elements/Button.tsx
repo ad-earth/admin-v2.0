811cd7958bafdeedd426624372382eecb868d6d4
sol <osunpine@gmail.com>
2023-04-07 01:08:20 +0900

    feat. 공통 button element 개발 #16
    - onClick 함수, type 추가
    - 컨벤션에 맞춰 className 수정

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -5,11 +5,19 @@ type TProps = {
   children?: React.ReactNode;
   text?: string;
   styleClass?: string;
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  onClick?: () => void;
 };
 
-export default function Button({ styleClass, children, text }: TProps) {
+export default function Button({
+  styleClass,
+  children,
+  text,
+  type,
+  onClick,
+}: TProps) {
   return (
-    <button className={styles[`${styleClass}`]}>
+    <button className={styles[`${styleClass}`]} type={type} onClick={onClick}>
       {text ? text : children}
     </button>
   );
