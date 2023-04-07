fbe1b418d1b74055a1259392625dd48c6559b5bc
damiiya <ddam2lee@gmail.com>
2023-04-08 01:33:24 +0900

    feat. 공통 스타일 컬러, 타입, 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,7 +7,21 @@ export const CATEGORY = [
   '화장품',
   '문구',
 ];
-
+export const PARCELSTATUS = ['전체', '신규주문', '배송완료', '주문취소'];
+export const SERVICEHEADLIST = [
+  'No',
+  '주문번호',
+  '상품번호',
+  '상품명',
+  '수량',
+  '아이디',
+  '수령인',
+  '주소',
+  '연락처',
+  '배송메시지',
+  '주문일자',
+  '배송상태',
+];
 export const ID_REG = /^[a-zA-Z0-9]{5,10}$/;
 export const PWD_REG =
   /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -5,3 +5,24 @@ export interface IIdResponse {
 export interface IPwdResponse {
   a_Idx: number;
 }
+
+export type TOptionList = {
+  color: string | null;
+  colorCode: string | null;
+  option: string | null;
+  optionPrice: number | null;
+  optionCnt: number | null;
+};
+export interface IProdResponse {
+  p_Category: string;
+  p_Thumbnail: string[];
+  p_Name: string;
+  p_Cost: number;
+  p_Sale: boolean;
+  p_Discount: number;
+  p_Option: TOptionList[];
+  p_Desc: string;
+  p_Content: string;
+}
+
+export type TError = { errorMessage: string };
