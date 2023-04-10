8ae8f308ac75e3152bd2bd25592adf146bfa0462
damiiya <ddam2lee@gmail.com>
2023-04-10 20:21:54 +0900

    feat. 상품 등록 페이지 개발 #10
    - 상품 조회 요청 추가

---
+++ src/query/useGetProduct.ts
@@ -0,0 +1,36 @@
+import type { AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
+import { useSetRecoilState } from 'recoil';
+import queryKeys from '../constants/queryKeys';
+import { getProdInfo } from '../shared/api/apis';
+import { optionListState } from '../store/option';
+import type { IProdResponse, TOptionList } from './../shared/types/types';
+
+const useGetProduct = (p_No: number) => {
+  const list = useSetRecoilState(optionListState);
+  const { data: prodData } = useQuery<AxiosResponse<IProdResponse>, Error>(
+    [queryKeys.PRODUCT_INFO],
+    () => getProdInfo(p_No),
+    {
+      refetchOnWindowFocus: false,
+      enabled: !!p_No,
+      onSuccess: ({ data }) => {
+        let result = data.p_Option;
+        let resultList = result.map((item: TOptionList, idx: number) => ({
+          id: idx + 1,
+          colorCheck: item.color ? true : false,
+          optionCheck: item.option ? true : false,
+          color: item.color,
+          colorCode: item.colorCode,
+          option: item.option,
+          optionPrice: item.optionPrice,
+          optionCnt: item.optionCnt,
+        }));
+        list(resultList);
+      },
+    }
+  );
+  return { prodData };
+};
+
+export default useGetProduct;
