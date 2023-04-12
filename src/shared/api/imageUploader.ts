6c9e0d622f2fdc3ed4b980d2d2306b96b27d973f
damiiya <ddam2lee@gmail.com>
2023-04-12 16:42:31 +0900

    feat. 이미지 요청 유틸 함수 개선

--- src/shared/api/imageUploader.ts
+++ src/shared/api/imageUploader.ts
@@ -1,27 +1,21 @@
 import ReactS3Client from 'react-aws-s3-typescript';
-import type { IConfig } from '../../../node_modules/react-aws-s3-typescript/dist/types';
+import type { IConfig } from 'react-aws-s3-typescript/dist/types';
 import { editorConfig, thumbnailConfig } from '../utils/s3Config';
 
-export async function thumbnailUploader(file: any) {
-  const s3 = new ReactS3Client(thumbnailConfig as IConfig);
-  const fileList = file;
+const uploadFile = async (file: any, config: IConfig) => {
+  const s3 = new ReactS3Client(config);
   try {
-    return await s3
-      .uploadFile(fileList[0], fileList[0].name)
-      .then(data => data.location);
+    const data = await s3.uploadFile(file, file.name);
+    return data.location;
   } catch (err) {
     console.error(err);
   }
-}
+};
 
-export async function editorUploader(file: any) {
-  const s3 = new ReactS3Client(editorConfig as IConfig);
-  const fileList = file;
-  try {
-    return await s3
-      .uploadFile(fileList[0], fileList[0].name)
-      .then(data => data.location);
-  } catch (err) {
-    console.error(err);
-  }
-}
+export const thumbnailUploader = async (file: any) => {
+  return uploadFile(file[0], thumbnailConfig);
+};
+
+export const editorUploader = async (file: any) => {
+  return uploadFile(file[0], editorConfig);
+};
