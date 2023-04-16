abe1b6bbf0106cc545a0406acb70ae8d4c449378
sol <osunpine@gmail.com>
2023-04-17 08:59:53 +0900

    refactor. 로그인 리팩토링 #59
    - response type 추가
    - 로그인 form data만으로 처리하도록 변경

--- src/query/useLogin.ts
+++ src/query/useLogin.ts
@@ -1,17 +1,25 @@
+import type { AxiosError, AxiosResponse } from 'axios';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../shared/api/apis';
+import type { TError } from '../shared/types/types';
 
-const useLogin = (id: string, password: string) => {
+type TData = {
+  id: string;
+  pwd: string;
+};
+
+const useLogin = () => {
   const navigate = useNavigate();
-  const { mutate } = useMutation(() => login(id, password), {
-    onSuccess: data => {
-      localStorage.setItem('adminToken', data?.data.token);
-      navigate('/');
-    },
-  });
-
-  return { mutate };
+  return useMutation<AxiosResponse, AxiosError<TError>, any, unknown>(
+    ({ id, pwd }: TData) => login(id, pwd),
+    {
+      onSuccess: data => {
+        localStorage.setItem('adminToken', data?.data.token);
+        navigate('/');
+      },
+    }
+  );
 };
 
 export default useLogin;
