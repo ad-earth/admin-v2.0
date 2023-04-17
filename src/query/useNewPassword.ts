83c90bb698efee1501e59d3a478b3c4bf3b54d00
sol <osunpine@gmail.com>
2023-04-17 16:17:28 +0900

    fix. 계정 찾기 캐시 데이터 삭제

--- src/query/useNewPassword.ts
+++ src/query/useNewPassword.ts
@@ -1,14 +1,17 @@
 import { toast } from 'react-hot-toast';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
+import queryKeys from '../constants/queryKeys';
 import { resetPwd } from '../shared/api/apis';
 
 const useNewPassword = (confirmId: number, newPassword: string) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate } = useMutation(() => resetPwd(confirmId, newPassword), {
     onSuccess: () => {
       toast.success('비밀번호가 변경되었습니다.');
       navigate('/login');
+      queryClient.invalidateQueries(queryKeys.FIND_PWD);
     },
   });
 
